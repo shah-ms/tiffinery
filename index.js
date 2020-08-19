175c614640a6a237c812a3e4cb1bbb41df1fb29f
Mehul Shah <msshah985@gmail.com>
2020-08-20 00:13:34 +0530

    Server changes

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
-const port = 5000;
 const router = require("./router/user");
 const bodyParser = require('body-parser');
 const app = express();
-const hostname = "172.19.0.1";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use(router);
+app.use("https://mighty-temple-44269.herokuapp.com", router);
+
+let port = process.env.PORT;
+
+if(port == null || port == "") {
+	port = 5000;
+}
 
 app.listen(port,() => {
   console.log(`Server running on port : ${port}`);
