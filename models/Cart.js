4fb429d24d9bef563592a5aeff879f62f71317ae
Mehul Shah <msshah985@gmail.com>
2020-09-02 12:11:18 +0530

    Cart added

--- models/Cart.js
+++ models/Cart.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const CartSchema = new mongoose.Schema({
   tiffin: {
     type: String,
+    required: true,
   },
   userId: {
     type: String,
+    required: true,
   },
   quantity: {
     type: Number,
