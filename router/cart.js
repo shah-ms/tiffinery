c2285e40df1935b6389eb414a577b8a29e098fa1
Mehul Shah <msshah985@gmail.com>
2020-09-08 17:57:55 +0530

    Cart Added

--- router/cart.js
+++ router/cart.js
@@ -35,7 +35,7 @@ router.post("/cart", async (req, res) => {
       } else {
         let cartId = result["cartId"];
         var sql =
-          "INSERT INTO cartItems (cartId, tiffinId, quantity) VALUES (?)";
+          "INSERT INTO cartItems (cartId, tiffinId, quantity) VALUES (?, ?, ?)";
         conn.query(sql, [cartId, tiffin, quantity], (err, result) => {
           if (err) {
             res.status(400).json(err);
