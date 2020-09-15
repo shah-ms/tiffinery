75627ffd10b7c7303812947e6653d44e1faad1b2
Mehul Shah <msshah985@gmail.com>
2020-09-15 12:04:04 +0530

    Cart features

--- router/cart.js
+++ router/cart.js
@@ -3,48 +3,68 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 const conn = require("../db/db");
 
+function getCartItems(cartId) {
+  var sql =
+    "SELECT * FROM cartItems INNER JOIN tiffins ON cartItems.tiffinId = tiffins.tiffinId WHERE cartItems.cartId=?";
+  conn.query(sql, cartId, (err, result) => {
+    if (!err) {
+      return result;
+    }
+  });
+}
+
 router.post("/cart", async (req, res) => {
   //   const token = event.headers["Authorization"].replace("Bearer ", "");
   //   const payload = jwt.verify(token, process.env.ACCESS_TOKEN_KEY);
   //   const userId = payload["userId"];
-  const { tiffin, quantity } = req.body;
-  console.log(tiffin);
+  const { tiffinId, quantity } = req.body;
+  console.log(tiffinId, quantity);
 
   var sql = "SELECT cartId FROM cart WHERE userId=?";
   conn.query(sql, 1, (err, result) => {
     if (err) {
     } else {
-      if (result.length == 0) {
-        console.log(result, "New Cart");
-        var sql = "INSERT INTO cart (userId) VALUES (?)";
-        conn.query(sql, 1, (err, result) => {
-          if (err) {
-            res.status(400).json(err);
-          } else {
-            let cartId = result["insertId"];
-            var sql =
-              "INSERT INTO cartItems (cartId, tiffinId, quantity) VALUES (?, ?, ?)";
-            conn.query(sql, [cartId, tiffin, quantity], (err, result) => {
-              if (err) {
-                res.status(400).json(err);
-              } else {
-                res.status(200).json("Added to Cart");
-              }
-            });
-          }
-        });
-      } else {
-        console.log(result, "Existing cart");
+      if (result.length == 1) {
         let cartId = result[0]["cartId"];
-        var sql =
-          "INSERT INTO cartItems (cartId, tiffinId, quantity) VALUES (?, ?, ?)";
-        conn.query(sql, [cartId, tiffin, quantity], (err, result) => {
-          if (err) {
-            res.status(400).json(err);
-          } else {
-            res.status(200).json("Added to Cart");
-          }
-        });
+
+        if (quantity == 1) {
+          var sql =
+            "UPDATE cartItems SET quantity = quantity + 1 WHERE cartId=? AND tiffinId=?";
+          conn.query(sql, [cartId, tiffinId], async (err, result) => {
+            if (err) {
+            } else {
+              let cartItems = await getCartItems(cartId);
+              res.status(200).json(cartItems);
+            }
+          });
+        } else {
+          var sql =
+            "SELECT cartItems.quantity FROM cartItems WHERE cartId=? AND tiffinId=?";
+          conn.query(sql, [cartId, tiffinId], (err, result) => {
+            if (!err) {
+              if (result["quantity"] == 1) {
+                var sql = "DELETE FROM cartItems WHERE cartId=? AND tiffinId=?";
+                conn.query(sql, [cartId, tiffinId], async (err, result) => {
+                  if (!err) {
+                    let cartItems = await getCartItems(cartId);
+                    res.status(200).json(cartItems);
+                  }
+                });
+              } else {
+                var sql =
+                  "UPDATE cartItems SET quantity = quantity - 1 WHERE cartId=? AND tiffinId=?";
+                conn.query(sql, [cartId, tiffinId], async (err, result) => {
+                  if (err) {
+                  } else {
+                    let cartItems = await getCartItems(cartId);
+                    res.status(200).json(cartItems);
+                  }
+                });
+              }
+            }
+          });
+        }
+      } else {
       }
     }
   });
@@ -53,7 +73,7 @@ router.post("/cart", async (req, res) => {
 router.get("/getCart", async (req, res) => {
   try {
     var sql =
-      "SELECT cartItems.tiffinId, cartItems.quantity FROM cart INNER JOIN cartItems ON cart.cartId = cartItems.cartId WHERE cart.userId=?";
+      "SELECT * FROM cartItems INNER JOIN cart ON cartItems.cartId = cart.cartId INNER JOIN tiffins ON cartItems.tiffinId = tiffins.tiffinId WHERE cart.userId=?";
     conn.query(sql, 1, (err, result) => {
       if (err) {
         res.status(400).json(err);
